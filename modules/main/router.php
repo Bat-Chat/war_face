8883c81525fbc3d0d8b8a8352472db2c4d029eee
BatChat <batchatillon.t@gmail.com>
2015-02-17 16:58:14 +0200

    верстка

--- modules/main/router.php
+++ modules/main/router.php
@@ -14,7 +14,14 @@ if (isset($_SESSION['vhod']))
 	exit();
 }
 
-ob_start();
-include IRB_ROOT.'skins/'.IRB_SKIN.'/tpl/main/main.html';
-$content = ob_get_contents();
-ob_end_clean();
+if($_GET['page'] == 'download') {
+	ob_start();
+	include IRB_ROOT.'skins/'.IRB_SKIN.'/tpl/main/download.html';
+	$content = ob_get_contents();
+	ob_end_clean();
+} else {
+	ob_start();
+	include IRB_ROOT.'skins/'.IRB_SKIN.'/tpl/main/main.html';
+	$content = ob_get_contents();
+	ob_end_clean();
+}
